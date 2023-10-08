4d47a8e11315bf7937e7a146f9db8a938963bb12
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2023-10-08 13:03:46 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.75.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# resource "random_integer" "ri" {
+#   min = 10000
+#   max = 99999
+# }
+
+resource "azurerm_resource_group" "taskBoardRG" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_mssql_server" "task-board-sql" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.taskBoardRG.name
+  location                     = azurerm_resource_group.taskBoardRG.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "TaskBoardDB" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.task-board-sql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  sku_name       = "Basic"
+  zone_redundant = false
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.taskBoardRG.name
+  location            = azurerm_resource_group.taskBoardRG.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "linux_web_app" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.taskBoardRG.name
+  location            = azurerm_service_plan.service_plan.location
+  service_plan_id     = azurerm_service_plan.service_plan.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.task-board-sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.TaskBoardDB.name};User ID=${azurerm_mssql_server.task-board-sql.administrator_login};Password=${azurerm_mssql_server.task-board-sql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_app_service_source_control" "git_repo" {
+  app_id                 = azurerm_linux_web_app.linux_web_app.id
+  repo_url               = var.repo_Url
+  branch                 = "main"
+  use_manual_integration = true
+}
+
+resource "azurerm_mssql_firewall_rule" "allowAll" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.task-board-sql.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name in Azure"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Resource group location"
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "Azure app service plan name"
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "Azure app service name"
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "Azure SQL server name"
+}
+
+variable "sql_database_name" {
+  type        = string
+  description = "SQL database name"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "DB admin username"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "DB admin password"
+}
+
+variable "firewall_rule_name" {
+  type        = string
+  description = "Firewall rule name"
+}
+
+variable "repo_Url" {
+  type        = string
+  description = "Git repo URL"
+}
