c18bc4631dc2de800add5e7c767444ed6723fd3c
Ivan Kukutsov <ivankukucov@gmail.com>
2023-10-10 22:53:45 +0300

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.75.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "StorageRG" {
   name     = var.storage_resource_group_name
   location = var.resource_group_location
